40439c6a9ca85400464e5bcea6ddaf54675a8b71
wqt#0001 <77388034+wqt-dll@users.noreply.github.com>
2023-03-27 18:12:53 +0100

    Add files via upload

---
+++ properties.h
@@ -0,0 +1,15 @@
+class properties
+{
+    public:
+
+    int lines = INT32_MAX;
+    std::string getFile()
+    {
+        std::string fileName;
+        std::cout << "Enter file name: ";
+        std::cin >> fileName;
+        fileName += ".py";
+        return fileName;
+    }
+    std::string fileName = getFile();
+};
